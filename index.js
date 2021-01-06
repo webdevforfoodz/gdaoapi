5bb48295306d0df7945cb1f21705c02335a66d5b
webdevforfoodz <73584854+webdevforfoodz@users.noreply.github.com>
2021-01-06 02:45:30 -0800

    ha

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //Import routes
 import supply from './routes/supply.js'
